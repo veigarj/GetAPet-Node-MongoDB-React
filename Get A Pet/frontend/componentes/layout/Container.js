87faf32d38fdb92c168b798bf841bb8a1e4a8c2d
Glauco Veiga <101466539+veigarj@users.noreply.github.com>
2023-01-27 07:47:53 -0300

    Container

---
+++ Get A Pet/frontend/componentes/layout/Container.js
@@ -0,0 +1,13 @@
+import style from './layout/Container.module.css'
+
+function Container() {
+    return(
+        <section>
+            <h1>Container</h1>
+        </section>
+
+    )
+    
+}
+
+export default Container
---
+++ Get A Pet/frontend/componentes/layout/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return(
+        <footer>
+            <p>
+                <span className='bold'> Get a Pet</span> &copy; 2023
+            </p>
+        </footer>
+
+    )
+    
+}
+
+export default Footer

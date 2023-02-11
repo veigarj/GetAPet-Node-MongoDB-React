540876b5a07cce28ac6d89752d3adf70f9050131
Glauco Veiga <veiga.cg@gmail.com>
2023-02-11 13:17:42 -0300

    addPet

--- Get A Pet/frontend/src/components/pages/Pet/AddPet.js
+++ Get A Pet/frontend/src/components/pages/Pet/AddPet.js
@@ -2,7 +2,7 @@ import api from '../../utils/api';
 import styles from './AddPets.module.css';
 
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import PetForm from '../../form/PetForm';
 
@@ -10,13 +10,57 @@ import PetForm from '../../form/PetForm';
 import useFlashMessage from '../../hooks/useFlashMessage';
 
 const AddPet = () => {
+  // pegar o token do localstorage
+  const [token] = useState(localStorage.getItem('token') || '');
+  // usar flashMessage
+  const { setFlashMessage } = useFlashMessage();
+  const history = useNavigate();
+
+  async function registerPet(pet) {
+    let msgType = 'success';
+
+    const formData = new FormData();
+
+    // pega cada item do pet e joga em formData
+    await Object.keys(pet).forEach((key) => {
+      if (key === 'images') {
+        // percorrer rodas as imagens e joga dentro de um array [images]
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append('images', pet[key][i]);
+        }
+      } else {
+        // acessar o obj pelo nome da chave
+        formData.append(key, pet[key]);
+      }
+    });
+
+    // envia um post onde invia o formData no body(corpo da requisição) com o cabeçalho
+    const data = await api
+      .post('pets/create', formData, {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type': 'multipart/form-data',
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        msgType = 'error';
+        return err.response.data;
+      });
+
+    setFlashMessage(data.message, msgType);
+
+    if (msgType !== 'erro') {
+      history('/pets/mypets');
+    }
+  }
   return (
     <section className={styles.addpet_header}>
       <div>
         <h1>Cadastrar um Pet</h1>
         <p>Depois ele ficara disponivel para adoção</p>
       </div>
-      <PetForm btnText="Cadastrar Pet" />
+      <PetForm handleSubmit={registerPet} btnText="Cadastrar Pet" />
     </section>
   );
 };
